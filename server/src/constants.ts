173158d062389747eb978e8283f3a98ca5ca9957
Nguyễn Hữu Chung - CMCGlobal DU11 <nhchung@cmcglobal.vn>
2021-10-27 11:28:46 +0700

    change 1

--- server/src/constants.ts
+++ server/src/constants.ts
@@ -1,5 +1,5 @@
 export const __prod__ = process.env.NODE_ENV === "production";
-export const loginDurationTime = "30m";
+export const loginDurationTime = "20m";
 export const redisLoginDurationTime = 30 * 60;
 export const refresherTokenDurationTime = "24h";
 export const redisRefresherTokenDurationTime = 60 * 60 * 24;
